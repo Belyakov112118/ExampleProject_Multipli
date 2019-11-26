fe11f5f24454ca46f94ceef5c306adc2323091a0
Gff <pawelakkaunt@gmail.com>
2019-11-26 12:46:04 +0300

    Add back button

---
+++ ExampleProject_Multipli/SecondController.m
@@ -0,0 +1,35 @@
+//
+//  SecondController.m
+//  ExampleProject_Multipli
+//
+//  Created by Student on 26.11.2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+#import "SecondController.h"
+
+@interface SecondController ()
+
+@end
+
+@implementation SecondController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"Hi!");
+}
+- (IBAction)backTap:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
